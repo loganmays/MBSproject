79bfef172ad9cfcec2c7e21db95f4126135595b5
hgallina44 <118315405+hgallina44@users.noreply.github.com>
2022-11-15 16:26:56 -0600

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,2 @@
 print("braden likes semen")
+print("logan mays does unspeakable deeds with his microsoft go tablet. I know this because he did not have enough storage to open a tab so that means his computer is filled with...")
